00ceb0952cbcca861159b354f68f026d2834b5d6
Sophie1810 <s.e.woertman@student.utwente.nl>
2025-03-21 11:35:56 +0100

    General

--- MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/General.kt
+++ MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/General.kt
@@ -14,7 +14,19 @@ import furhatos.util.Language
 
 class AskAboutMasters: Intent() {
     override fun getExamples(lang: Language): List<String> {
-        return listOf("Tell me about master's programs", "I want to know about the Master programs", "What Master programs are available?")
+        return listOf("Tell me about master's programs", "I want to know about the Master programs", "What Master programs are available?", "I want help choosing a Masters")
+    }
+}
+
+class AskAboutBachelors: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I don't know what Masters I want", "I have no idea what Masters I want to do")
+    }
+}
+
+class AskAboutRequirements: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I want to know the requirements of a masters", "I want to know about a pre master", "I would like information about the requirements of a Master")
     }
 }
 
@@ -53,5 +65,14 @@ val FollowUpQuestion: State = state {
     onResponse<AskAboutMasters> {
         goto(MasterSelection)
     }
+
+    onResponse<AskAboutBachelors> {
+        goto(MasterSelection) //change to bachelor
+    }
+
+    onResponse<AskAboutRequirements> {
+        goto(MasterSelection) //change to requirements
+    }
+
 
 }
