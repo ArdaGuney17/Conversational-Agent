30f40e03aced8793e3c933e264e86e7cb80cdc16
Sophie1810 <s.e.woertman@student.utwente.nl>
2025-03-21 11:12:02 +0100

    Greetings

--- MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/greeting.kt
+++ MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/greeting.kt
@@ -10,29 +10,37 @@ import furhatos.nlu.common.Yes
 
 val Greeting: State = state(Parent) {
     onEntry {
-        furhat.say("Hello! My name is Matty, I will help you today finding your masters program")
-        furhat.ask("Do you want help choosing your masters?")
+        furhat.say("Hello! My name is Matty, I will help you today finding your master's program")
+        furhat.ask("Do you want help choosing your master's?")
     }
 
     onResponse<Yes> {
         furhat.say("Great!")
-        furhat.ask("Do you already have something in mind?")
-
-        onResponse<Yes> {
-            furhat.say("Good to hear!")
-            goto(MasterSelection) // User has something in mind
-        }
-
-        onResponse<No> {
-            furhat.say("That is okay")
-            goto(MasterSelection) // Change to bachelor
-        }
+        goto(AskIfSomethingInMind) // Transition to the next state
     }
 
     onResponse<No> {
         furhat.say("No worries! Thank you for this short conversation")
         goto(Idle)
     }
-
 }
+
+// New state to ask about preferences
+val AskIfSomethingInMind: State = state(Parent) {
+    onEntry {
+        furhat.ask("Do you already have something in mind?")
+    }
+
+    onResponse<Yes> {
+        furhat.say("Good to hear!")
+        goto(MasterSelection) // Transition to MasterSelection
+    }
+
+    onResponse<No> {
+        furhat.say("That is okay")
+        goto(MasterSelection) // Transition to MasterSelection (or another state if needed)
+    }
+}
+
+
 
