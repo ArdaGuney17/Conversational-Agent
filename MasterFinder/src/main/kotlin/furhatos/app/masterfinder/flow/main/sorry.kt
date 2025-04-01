81d2a1bb26a8e88bf8a0f5e6f27c97cd4c28652a
ArdaGuney17 <71980713+ArdaGuney17@users.noreply.github.com>
2025-04-01 10:39:06 +0200

    Update sorry.kt

--- MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/sorry.kt
+++ MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/sorry.kt
@@ -6,7 +6,7 @@ import furhatos.flow.kotlin.*
 val Sorry: State = state(Parent) {
 
     onEntry {
-        furhat.say("Sorry, I do not know this. For more information take a look at ... or ask a person around me.")
+        furhat.say("Sorry, I do not know this. For more information take a look at the website of University of Twente or ask a person around me.")
         goto(Idle)
     }
 }
