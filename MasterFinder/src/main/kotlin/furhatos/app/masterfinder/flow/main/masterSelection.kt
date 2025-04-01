a89bb12aa7a7f8bdd3d9d4bc13c351700a477279
Sophie1810 <s.e.woertman@student.utwente.nl>
2025-04-01 09:23:46 +0200

    userAsnwers

--- MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/masterSelection.kt
+++ MasterFinder/src/main/kotlin/furhatos/app/masterfinder/flow/main/masterSelection.kt
@@ -8,45 +8,7 @@ import furhatos.nlu.common.*
 import furhatos.util.Language
 
 // Define custom intents
-class AdmissionRequest : Intent()
-class CareerRequest : Intent()
-class StructureRequest : Intent()
 
-class MasterCourses: Intent() {
-    override fun getExamples(lang: Language): List<String> {
-
-        val masterStudies = listOf("Applied Mathematics", "Applied Physics", "Biomedical Engineering",
-                "Business Administration", "Business Information Technology",
-                "Chemical Science and Engineering", "Civil Engineering and Management",
-                "Communication Science", "Computer Science", "Construction Management and Engineering",
-                "Educational Science and Technology", "Electrical Engineering", "Embedded Systems",
-                "Environmental and Energy Management", "European Studies",
-                "Geo-Information Science and Earth Observation",
-                "Geographical Information Management and Applications", "Health Sciences",
-                "Industrial Design Engineering", "Industrial Engineering and Management",
-                "Interaction Technology", "Internet Science and Technology", "Mechanical Engineering",
-                "Nanotechnology", "Philosophy of Science, Technology and Society", "Psychology",
-                "Public Administration", "Robotics", "Spatial Engineering",
-                "Sustainable Energy Technology", "Technical Medicine")
-
-        return masterStudies
-    }
-
-    fun processUserInput(userInput: String) {
-        val masterCourses = getExamples(Language.ENGLISH_GB)
-
-        for (study in masterCourses) {
-            if (userInput.contains(study, ignoreCase = true)) {
-                // Save the study program to the UserData singleton
-                UserData.userOptionalMaster= study
-                break
-            }
-        }
-
-        // Log the study program (just for demo)
-        println("User is studying: ${UserData.userOptionalMaster}")
-    }
-}
 
 
 
